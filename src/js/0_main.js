6a64dcb0b212e8fc3a8662aabe1b3cafe3e9bd53
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-12-23 23:00:56 +0100

    added new photo

--- src/js/0_main.js
+++ src/js/0_main.js
@@ -4,10 +4,17 @@ console.log('holis')
 
 const hamburger = document.querySelector ('.js-hamburger');
 const menu = document.querySelector ('.js-menuH');
+const infoH = document.querySelector ('.js-infoHang');
 
 
-const handleInput = () =>{
+const handleInput = (event) =>{
+    event.preventDefault();
     menu.classList.toggle('hidden');
 }
 
+const handleInfoH = (event) =>{
+    event.preventDefault();
+}
+
 hamburger.addEventListener('click', handleInput);
+infoH.addEventListener('click', handleInfoH);
