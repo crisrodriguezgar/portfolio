3c574d469aabf6982e535845a444c3672370de35
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2024-01-19 13:18:27 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,2 @@
-console.log(">> Ready :)");
+console.log("holis");const e=document.querySelector(".js-hamburger"),n=document.querySelector(".js-menuH");document.querySelector(".js-infoHang");const o=()=>{n.classList.toggle("hidden")};e.addEventListener("click",o);
 //# sourceMappingURL=main.js.map
