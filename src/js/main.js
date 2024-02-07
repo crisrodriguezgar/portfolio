1e07b406d516d9d8905b426aee978052ab35644c
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2024-02-07 14:35:23 +0100

    update scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,19 @@
 'use strict';
 
-console.log('holis');
-
 const hamburger = document.querySelector('.js-hamburger');
 const menu = document.querySelector('.js-menuH');
-const infoH = document.querySelector('.js-infoHang');
+const links = document.querySelectorAll('.headerH__textH');
+const checkbox = document.querySelector('.js-hamburger');
 
 const handleInput = () => {
   menu.classList.toggle('hidden');
 };
 
 hamburger.addEventListener('click', handleInput);
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.add('hidden');
+    checkbox.checked = false;
+  });
+});
