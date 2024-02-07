83289fb8972ebbcd0b453bb29864b5b8a2e30b88
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2024-02-07 14:35:47 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,2 @@
-console.log("holis");const e=document.querySelector(".js-hamburger"),n=document.querySelector(".js-menuH");document.querySelector(".js-infoHang");const o=()=>{n.classList.toggle("hidden")};e.addEventListener("click",o);
+const t=document.querySelector(".js-hamburger"),e=document.querySelector(".js-menuH"),n=document.querySelectorAll(".headerH__textH"),r=document.querySelector(".js-hamburger"),s=()=>{e.classList.toggle("hidden")};t.addEventListener("click",s);n.forEach(c=>{c.addEventListener("click",()=>{e.classList.add("hidden"),r.checked=!1})});
 //# sourceMappingURL=main.js.map
