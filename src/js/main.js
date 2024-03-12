c37cd2217faabf79a2436f78c71b571af92e69dd
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2024-03-12 19:15:38 +0100

    added scroll up

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,13 @@
 const hamburger = document.querySelector('.js-hamburger');
 const menu = document.querySelector('.js-menuH');
 const links = document.querySelectorAll('.headerH__textH');
-const checkbox = document.querySelector('.js-hamburger');
 
-const flagsElement = document.getElementById('flags');
-const languageSwitch = document.getElementById('languageSwitch');
+const languageSwitch = document.querySelector('.languageSwitch');
 const textsToChange = document.querySelectorAll('[data-section]');
-const flagsMobile = document.getElementById('flags-mobile');
-const languageSwitchMobile = flagsMobile.querySelector('#languageSwitch');
+
+const buttonUp = document.getElementById("button-up");
+
+/** hambuger **/
 
 const handleInput = () => {
   menu.classList.toggle('hidden');
@@ -20,9 +20,11 @@ hamburger.addEventListener('click', handleInput);
 links.forEach((link) => {
   link.addEventListener('click', () => {
     menu.classList.add('hidden');
-    checkbox.checked = false;
+    hamburger.checked = false;
   });
 });
+
+/** change languge **/
 
 const changeLanguage = async (language) => {
   const requestJson = await fetch(`./languages/${language}.json`);
@@ -36,30 +38,33 @@ const changeLanguage = async (language) => {
   }
 };
 
-/*desktop*/
-
-flagsElement.addEventListener('click', async (e) => {
-  if (e.target.tagName === 'P') {
-    const language = e.target.dataset.language;
-    await changeLanguage(language);
-  }
-});
-
 languageSwitch.addEventListener('change', async () => {
   const language = languageSwitch.checked ? 'en' : 'es';
   await changeLanguage(language);
 });
 
-/*mobile*/
 
-flagsMobile.addEventListener('click', async (e) => {
-  if (e.target.tagName === 'P') {
-    const language = e.target.dataset.language;
-    await changeLanguage(language);
+/** Scroll **/
+
+function scrollUp(){
+    const currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo(0, currentScroll - (currentScroll / 10));
+    }
+}
+
+buttonUp.addEventListener("click", scrollUp);
+
+
+window.addEventListener('scroll', function() {
+  const scroll = document.documentElement.scrollTop;
+
+  if (scroll > 650) {
+      buttonUp.style.display = "block";
+  } else {
+      buttonUp.style.display = "none";
   }
 });
 
-languageSwitchMobile.addEventListener('change', async () => {
-  const language = languageSwitchMobile.checked ? 'en' : 'es';
-  await changeLanguage(language);
-});
